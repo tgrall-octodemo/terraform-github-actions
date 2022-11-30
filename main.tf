ef4c29e3014a5aa7f8b4d83d8d82847eafbe105b
Tugdual Grall <tgrall@github.com>
2022-11-30 09:36:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
+    resource_group_name  = "rg-tgrall-demo-tf"
+    storage_account_name = "tgrallbackendterraform"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
